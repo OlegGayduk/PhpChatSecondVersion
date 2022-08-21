138829a0de165c8d5fa4a564f93510eef5fe91b4
OlegGayduk <63071210+OlegGayduk@users.noreply.github.com>
2022-08-21 16:06:02 +0300

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1,8 +1,6 @@
 ﻿<?php
 function connection() {
     $db = new mysqli('localhost', 'root', '','chat');
-    //$db = new mysqli('localhost', 'Oleg_blog', 'rembo123','test');
-        //$connection = mysqli_select_db('test',$db);
 
     if (mysqli_connect_errno()) {
        printf("Не удалось подключиться к базе данных. Код ошибки: %s\n",mysqli_connect_error());
